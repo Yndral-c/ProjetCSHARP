ce9a10efa428b7af97420df178a6646bde4d3aeb
Théotim ALBERTEAU <sunshouwu@outlook.com>
2025-10-31 13:59:31 +0100

    ICarRepository modifications 2

--- ProjetCS/Data/InterfaceRepository/ICarRepository.cs
+++ ProjetCS/Data/InterfaceRepository/ICarRepository.cs
@@ -2,10 +2,10 @@ namespace ProjetCS.Model;
 
 public class ICarRepository
 {
-    Console.log("1) Voir liste Voitures");
-    Console.log("2) Historique d'achats");
-    Console.log("3) Ajouter un client");
-    Console.log("4) Ajouter une voiture");
-    Console.log("5) Faire un achat de voiture");
-    Console.log("6) Fin");
+    Console.WriteLine("1) Voir liste Voitures");
+    Console.WriteLine("2) Historique d'achats");
+    Console.WriteLine("3) Ajouter un client");
+    Console.WriteLine("4) Ajouter une voiture");
+    Console.WriteLine("5) Faire un achat de voiture");
+    Console.WriteLine("6) Fin");
 }
