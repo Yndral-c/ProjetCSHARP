885d7d537501a6265e8e41e65bee76b48bf0132e
Yndral-c <landry.cotillon@gmail.com>
2025-10-31 10:16:29 +0100

    add cars

--- ProjetCS/Model/Cars.cs
+++ ProjetCS/Model/Cars.cs
@@ -2,5 +2,49 @@
 
 public class Car
 {
+    [Key]
+    public Guid Id { get; set; } = new Guid();
+
+    [Required] private string brand;
+
+    [Required] private string model;
+
+    [Required] private string year;
+
+    [Required] private double priceHt;
+
+    [Required] private string color;
+
+    [Required] private bool sale;
     
+    public string Brand
+    {
+        get => brand;
+        set => brand = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public string Model
+    {
+        get => model;
+        set => model = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public string Year
+    {
+        get => year;
+        set => year = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public double PriceHt
+    {
+        get => priceHt;
+        set => priceHt = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public string Color
+    {
+        get => color;
+        set => color = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public bool Sale
+    {
+        get => sale;
+        set => sale = value ?? throw new ArgumentNullException(nameof(value));
+    }
 }
