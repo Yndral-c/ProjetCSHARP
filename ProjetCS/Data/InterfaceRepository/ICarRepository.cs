559c7abd4fdf849b97f137363344eb5eea33da95
Yndral-c <landry.cotillon@gmail.com>
2025-10-31 14:28:35 +0100

    Remove console.writeline in ICarRepository

--- ProjetCS/Data/InterfaceRepository/ICarRepository.cs
+++ ProjetCS/Data/InterfaceRepository/ICarRepository.cs
@@ -2,10 +2,5 @@ namespace ProjetCS.Model;
 
 public class ICarRepository
 {
-    Console.WriteLine("1) Voir liste Voitures");
-    Console.WriteLine("2) Historique d'achats");
-    Console.WriteLine("3) Ajouter un client");
-    Console.WriteLine("4) Ajouter une voiture");
-    Console.WriteLine("5) Faire un achat de voiture");
-    Console.WriteLine("6) Fin");
+
 }
