46c33b0b40626a45f01dfe6b348b19b88edb809d
Yndral-c <landry.cotillon@gmail.com>
2025-10-31 10:49:32 +0100

    add Nuget extensions

--- ProjetCS/Model/Customers.cs
+++ ProjetCS/Model/Customers.cs
@@ -2,5 +2,42 @@
 
 public class Customers
 {
+    [Key]
+    public Guid Id { get; set; } = new Guid();
     
+    [Required] private string name;
+    
+    [Required] private string firstname;
+    
+    [Required] private DateTime birthdate;
+    
+    [Required] private string phoneNumber;
+    
+    [Required] private string email;
+    
+    public string Name
+    {
+        get => name;
+        set => name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public string Firstname
+    {
+        get => firstname;
+        set => firstname = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public string Birthdate
+    {
+        get => birthdate;
+        set => birthdate = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public string PhoneNumber
+    {
+        get => phoneNumber;
+        set => phoneNumber = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public string Email
+    {
+        get => email;
+        set => email = value ?? throw new ArgumentNullException(nameof(value));
+    }
 }
